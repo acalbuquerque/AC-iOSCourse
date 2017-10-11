d1ffea5586ab6485e2a4dfe4d9932e55d6630b5a
Jonas Alves Simoes <jsimoes@avenuecode.com>
2017-10-11 17:48:37 -0300

    life cycle project

---
+++ LifeCycleApp/LifeCycleApp/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  LifeCycleApp
+//
+//  Created by Avenue Code on 10/11/17.
+//  Copyright © 2017 lifecycleCourse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var redView: UIView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("1")
+        print(self.redView.frame)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("2")
+        print(self.redView.frame)
+        // Called when the view is about to made visible. Default does nothing
+
+
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("3")
+        print(self.redView.frame)
+        // Called when the view has been fully transitioned onto the screen. Default does nothing
+        //Layout configurations should be done here, now you have perfectly frame defined
+
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("4")
+        // Called when the view is dismissed, covered or otherwise hidden. Default does nothing
+
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("5")
+        // Called after the view was dismissed, covered or otherwise hidden. Default does nothing
+
+    }
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
